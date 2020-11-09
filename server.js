c704a20d8409c3b5cde7e65695dca90382051b4c
NyxOfDarkness <ibelieve.as@gmail.com>
2020-11-09 12:51:42 -0800

    staging for heroku

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.get('/location', handleLocation);
+
 function handleLocation(request, response) {
   try {
     let geoData = require('./data/location.json');
@@ -17,6 +20,38 @@ function handleLocation(request, response) {
     console.error(error);
   }
 }
+
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+app.get('/WeatherLocation', handleweather);
+
+function handleWeather(request, response) {
+  try {
+    let weatherData = require('./data/weather.json');
+    let city = request.query.city;
+    let locationWeather = new WeatherLocation(city, weatherData);
+    response.send(locationWeather);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function WeatherLocation(city, weatherData) {
+  this.search_query = city;
+  this.formatted_query = weatherData[0].city_name;
+  this.latitude = weatherData[0].lat;
+  this.longitude = weatherData[0].lon;
+}
+
+
+app.use('*', (request, response) => {
+  response.status(404).send('sorry, not found!');
+})
 
 app.listen(PORT, () => {
   console.log('server up');
